938ded3f979c80a8c6c3767806fec1d292547696
pauleyy <pauletteconstantino@gmail.com>
2014-11-24 23:01:49 +0800

    Skeletonization
    Codes for skeletonization.

--- Seight/Seight/ISkeletonize.h
+++ Seight/Seight/ISkeletonize.h
@@ -3,15 +3,20 @@
 
 //#include "stdafx.h"
 #include <vector>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 using namespace std;
+using namespace cv;
 
 class ISkeletonize
 {
 public:
 	ISkeletonize();
 	~ISkeletonize();
-	void perform(); //vector<int>
+	void perform(Mat img); //vector<int>
+    void thinningIteration(Mat& im, int iter);
+    void thinning(Mat& im);
 };
 
 #endif
