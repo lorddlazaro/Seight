ac020a5648a7d4b5d82e23d849e0cb4fed1d1468
pauleyy <pauletteconstantino@gmail.com>
2015-02-25 11:33:57 +0800

    Fix conflict

--- Seight/Seight/PlantModeler.cpp
+++ Seight/Seight/PlantModeler.cpp
@@ -8,41 +8,41 @@ using namespace std;
 
 void PlantModeler::processImage(Mat image, string filename) //PhenotypicData
 {
-	ofstream myfile;
-	
-	
+    ofstream myfile;
+    
+    
     //cout << "processing image" << endl;
     PlantModeler::correction->perform();
     Mat segmentedImage = PlantModeler::segmentation->perform(image);
     PlantModeler::imageFiltering->perform(segmentedImage);
     Mat edge = PlantModeler::edgeDetection->perform(segmentedImage);
     Mat skeleton = PlantModeler::skeletonization->perform(segmentedImage);
-	myfile.open("D:/De La Salle University/Work/Programming/Seight/Seight/Seight/tillerResults.txt", ios_base::app);
+    myfile.open("D:/De La Salle University/Work/Programming/Seight/Seight/Seight/tillerResults.txt", ios_base::app);
     int tiller = PlantModeler::tillerCount->perform(edge, segmentedImage);
-	myfile << tiller << "\n";
-	myfile.close();
-	myfile.open("D:/De La Salle University/Work/Programming/Seight/Seight/Seight/heightResults.txt", ios_base::app);
+    myfile << tiller << "\n";
+    myfile.close();
+    myfile.open("D:/De La Salle University/Work/Programming/Seight/Seight/Seight/heightResults.txt", ios_base::app);
     double height = PlantModeler::measureHeight(skeleton);
-	myfile << height << "\n";
-	myfile.close();
-	string hsvDirectory = "D:/DE LA SALLE UNIVERSITY/Work/Programming/Seight/Seight/Seight/image/HSV/";
-	string imageFile = "";
-	imageFile.append(hsvDirectory);
-	imageFile.append(filename);
-	cout << imageFile << endl;
-	imwrite(imageFile, segmentedImage);
-	string edgeDirectory = "D:/DE LA SALLE UNIVERSITY/Work/Programming/Seight/Seight/Seight/image/Edge/";
-	imageFile = "";
-	imageFile.append(edgeDirectory);
-	imageFile.append(filename);
-	cout << imageFile << endl;
-	imwrite(imageFile, edge);
-	string skeletonDirectory = "D:/DE LA SALLE UNIVERSITY/Work/Programming/Seight/Seight/Seight/image/Skeleton/";
-	imageFile = "";
-	imageFile.append(skeletonDirectory);
-	imageFile.append(filename);
-	cout << imageFile << endl;
-	imwrite(imageFile, skeleton);
+    myfile << height << "\n";
+    myfile.close();
+    string hsvDirectory = "D:/DE LA SALLE UNIVERSITY/Work/Programming/Seight/Seight/Seight/image/HSV/";
+    string imageFile = "";
+    imageFile.append(hsvDirectory);
+    imageFile.append(filename);
+    cout << imageFile << endl;
+    imwrite(imageFile, segmentedImage);
+    string edgeDirectory = "D:/DE LA SALLE UNIVERSITY/Work/Programming/Seight/Seight/Seight/image/Edge/";
+    imageFile = "";
+    imageFile.append(edgeDirectory);
+    imageFile.append(filename);
+    cout << imageFile << endl;
+    imwrite(imageFile, edge);
+    string skeletonDirectory = "D:/DE LA SALLE UNIVERSITY/Work/Programming/Seight/Seight/Seight/image/Skeleton/";
+    imageFile = "";
+    imageFile.append(skeletonDirectory);
+    imageFile.append(filename);
+    cout << imageFile << endl;
+    imwrite(imageFile, skeleton);
     //cout << "done processing" << endl;
 }
 
@@ -57,92 +57,92 @@ PlantModeler::~PlantModeler(){
 
 double PlantModeler::measureHeight(Mat skeleton)
 {
-	double pixelsIncm = 9;
-	double height;
-	double distance;
-	int b, g, r;
-	//unsigned char *input = (unsigned char*)(image.data);
-	bool highestFound = false, lowestFound = false;
-	int highx = 0, highy = 0, lowx = 0, lowy = 0;
-	
-	//cout << skeleton << endl;
-	cvtColor(skeleton, skeleton, CV_GRAY2BGR);
-
-	/*find highest point*/
-	//imshow("SKEL", skeleton);
-	waitKey(0);
-	//cout << "rows "; cout << skeleton.rows << endl;
-	//cout << "cols "; cout << skeleton.cols << endl;
-
-	//cout << "Looking for highest" << endl;
-	waitKey(0);
-	for (int i = 0; i < skeleton.rows && !highestFound; i++)
-	{
-		for (int j = 0; j < skeleton.cols && !highestFound; j++)
-		{
-			//cout << skeleton.at<Vec3b>(i, j) << endl;
-			Vec3b pixel = skeleton.at<Vec3b>(i, j);
-			//cout << "i" << i;
-			//cout << "j" << j << endl;
-			//waitKey(0);
-			b = pixel.val[0];
-			g = pixel.val[1];
-			r = pixel.val[2];
-
-			//cout << "r:" << r << " g:" << g << " b:" << b << endl;
-
-			if (b == 255 && g == 255 && r == 255){
-				//cout << "yes" << endl;
-				highx = i;
-				highy = j;
-				highestFound = true;
-				skeleton.at<Vec3b>(i, j) = pixel;
-				//cout << "r:" << r << " g:" << g << " b:" << b << endl;
-				//cout << "highest found! " << i << " " << j << endl;
-				waitKey(0);
-			}
-			//else cout << "no" << endl;
-		}
-	}
-	//cout << "Looking for lowest" << endl;
-	//cout << "rows "; cout << skeleton.rows-1 << endl;
-	//cout << "cols "; cout << skeleton.cols-1 << endl;
-	waitKey(0);
-	
-	/*find lowest point*/
-	for (int i = skeleton.rows-1; i >= 0 && !lowestFound; i--)
-	{
-		for (int j = skeleton.cols-1; j >= 0 && !lowestFound; j--)
-		{
-			Vec3b pixel = skeleton.at<Vec3b>(i, j);
-			//cout << "i" << i;
-			//cout << "j" << j << endl;
-			b = pixel[0];
-			g = pixel[1];
-			r = pixel[2];
-			//cout << "r:" << r << " g:" << g << " b:" << b << endl;
-			if (b == 255 && g == 255 && r == 255){
-				lowx = i;
-				lowy = j;
-				lowestFound = true;
-				//cout << "r:" << r << " g:" << g << " b:" << b << endl;
-				//cout << "lowest found! " << i << " " << j << endl;
-				waitKey(0);
-			}
-		}
-	}
-
-	//imshow("COLOR", skeleton);
-	//waitKey(0);
-	double x = lowx - highx;
-	double y = lowy - highy;
-
-	distance = pow(x, 2) + pow(y, 2);           //calculating distance by euclidean formula
-	distance = sqrt(distance);
-
-	height = distance / 9; //convert to cm
-	
-	cout << "height " << height << endl;
-
+    double pixelsIncm = 9;
+    double height;
+    double distance;
+    int b, g, r;
+    //unsigned char *input = (unsigned char*)(image.data);
+    bool highestFound = false, lowestFound = false;
+    int highx = 0, highy = 0, lowx = 0, lowy = 0;
+    
+    //cout << skeleton << endl;
+    cvtColor(skeleton, skeleton, CV_GRAY2BGR);
+    
+    /*find highest point*/
+    //imshow("SKEL", skeleton);
+    waitKey(0);
+    //cout << "rows "; cout << skeleton.rows << endl;
+    //cout << "cols "; cout << skeleton.cols << endl;
+    
+    //cout << "Looking for highest" << endl;
+    waitKey(0);
+    for (int i = 0; i < skeleton.rows && !highestFound; i++)
+    {
+        for (int j = 0; j < skeleton.cols && !highestFound; j++)
+        {
+            //cout << skeleton.at<Vec3b>(i, j) << endl;
+            Vec3b pixel = skeleton.at<Vec3b>(i, j);
+            //cout << "i" << i;
+            //cout << "j" << j << endl;
+            //waitKey(0);
+            b = pixel.val[0];
+            g = pixel.val[1];
+            r = pixel.val[2];
+            
+            //cout << "r:" << r << " g:" << g << " b:" << b << endl;
+            
+            if (b == 255 && g == 255 && r == 255){
+                //cout << "yes" << endl;
+                highx = i;
+                highy = j;
+                highestFound = true;
+                skeleton.at<Vec3b>(i, j) = pixel;
+                //cout << "r:" << r << " g:" << g << " b:" << b << endl;
+                //cout << "highest found! " << i << " " << j << endl;
+                waitKey(0);
+            }
+            //else cout << "no" << endl;
+        }
+    }
+    //cout << "Looking for lowest" << endl;
+    //cout << "rows "; cout << skeleton.rows-1 << endl;
+    //cout << "cols "; cout << skeleton.cols-1 << endl;
+    waitKey(0);
+    
+    /*find lowest point*/
+    for (int i = skeleton.rows-1; i >= 0 && !lowestFound; i--)
+    {
+        for (int j = skeleton.cols-1; j >= 0 && !lowestFound; j--)
+        {
+            Vec3b pixel = skeleton.at<Vec3b>(i, j);
+            //cout << "i" << i;
+            //cout << "j" << j << endl;
+            b = pixel[0];
+            g = pixel[1];
+            r = pixel[2];
+            //cout << "r:" << r << " g:" << g << " b:" << b << endl;
+            if (b == 255 && g == 255 && r == 255){
+                lowx = i;
+                lowy = j;
+                lowestFound = true;
+                //cout << "r:" << r << " g:" << g << " b:" << b << endl;
+                //cout << "lowest found! " << i << " " << j << endl;
+                waitKey(0);
+            }
+        }
+    }
+    
+    //imshow("COLOR", skeleton);
+    //waitKey(0);
+    double x = lowx - highx;
+    double y = lowy - highy;
+    
+    distance = pow(x, 2) + pow(y, 2);           //calculating distance by euclidean formula
+    distance = sqrt(distance);
+    
+    height = distance / 9; //convert to cm
+    
+    cout << "height " << height << endl;
+    
     return height;
 }
--- Seight/Seight/defaultSkeletonize.cpp
+++ Seight/Seight/defaultSkeletonize.cpp
@@ -15,28 +15,28 @@ defaultSkeletonize::~defaultSkeletonize()
 
 Mat defaultSkeletonize::perform(Mat image)
 {
-	//cout << "perform skeletonization" << endl;
-
-	Mat bw;
-	cvtColor(image, bw, CV_BGR2GRAY);
-	threshold(bw, bw, 10, 255, CV_THRESH_BINARY);
-
-	thinning(bw);
+    //cout << "perform skeletonization" << endl;
+    
+    Mat bw;
+    cvtColor(image, bw, CV_BGR2GRAY);
+    threshold(bw, bw, 10, 255, CV_THRESH_BINARY);
+    
+    thinning(bw);
     //bitwise_not(bw, bw);
-
-	//imshow("Thinning", bw);
+    
+    //imshow("Thinning", bw);
     //waitKey(0);
-
-	//imwrite("D:/DE LA SALLE UNIVERSITY/Work/Programming/Seight/Seight/Seight/image/result_ZhangSuen.JPG", bw, vector < int > {0});
+    
+    //imwrite("D:/DE LA SALLE UNIVERSITY/Work/Programming/Seight/Seight/Seight/image/result_ZhangSuen.JPG", bw, vector < int > {0});
     //imwrite("/Users/pauletteconstantino/THESIS/structure3.JPG", bw);
     
-	return bw;
+    return bw;
 }
 
 void defaultSkeletonize::thinningIteration(Mat& im, int iter)
 {
     Mat marker = Mat::zeros(im.size(), CV_8UC1);
-
+    
     for (int i = 1; i < im.rows-1; i++)
     {
         for (int j = 1; j < im.cols-1; j++)
@@ -49,38 +49,38 @@ void defaultSkeletonize::thinningIteration(Mat& im, int iter)
             uchar p7 = im.at<uchar>(i+1, j-1);
             uchar p8 = im.at<uchar>(i, j-1);
             uchar p9 = im.at<uchar>(i-1, j-1);
-
-            int A  = (p2 == 0 && p3 == 1) + (p3 == 0 && p4 == 1) + 
-                     (p4 == 0 && p5 == 1) + (p5 == 0 && p6 == 1) + 
-                     (p6 == 0 && p7 == 1) + (p7 == 0 && p8 == 1) +
-                     (p8 == 0 && p9 == 1) + (p9 == 0 && p2 == 1);
+            
+            int A  = (p2 == 0 && p3 == 1) + (p3 == 0 && p4 == 1) +
+            (p4 == 0 && p5 == 1) + (p5 == 0 && p6 == 1) +
+            (p6 == 0 && p7 == 1) + (p7 == 0 && p8 == 1) +
+            (p8 == 0 && p9 == 1) + (p9 == 0 && p2 == 1);
             int B  = p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9;
             int m1 = iter == 0 ? (p2 * p4 * p6) : (p2 * p4 * p8);
             int m2 = iter == 0 ? (p4 * p6 * p8) : (p2 * p6 * p8);
-
+            
             if (A == 1 && (B >= 2 && B <= 6) && m1 == 0 && m2 == 0)
                 marker.at<uchar>(i,j) = 1;
         }
     }
-
+    
     im &= ~marker;
 }
 
 void defaultSkeletonize::thinning(Mat& im)
 {
-	im /= 255;
-
-	cv::Mat prev = cv::Mat::zeros(im.size(), CV_8UC1);
-	cv::Mat diff;
-
-	do {
-		//cout << "in thinning ";
-		thinningIteration(im, 0);
-		thinningIteration(im, 1);
-		cv::absdiff(im, prev, diff);
-		im.copyTo(prev);
-		//cout << countNonZero(diff) << endl;
-	} while (cv::countNonZero(diff) > 1000);
-
-	im *= 255;
+    im /= 255;
+    
+    cv::Mat prev = cv::Mat::zeros(im.size(), CV_8UC1);
+    cv::Mat diff;
+    
+    do {
+        //cout << "in thinning ";
+        thinningIteration(im, 0);
+        thinningIteration(im, 1);
+        cv::absdiff(im, prev, diff);
+        im.copyTo(prev);
+        //cout << countNonZero(diff) << endl;
+    } while (cv::countNonZero(diff) > 1000);
+    
+    im *= 255;
 }
