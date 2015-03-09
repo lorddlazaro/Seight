d1012e7acc673c537a7454e343b0fc8a50519489
ElishaG <elishajeremy.gonzales@gmail.com>
2015-03-09 19:51:07 +0800

    Changed equalizing histogram from SV values in HSV color space to Y in YCrCb color space.
    Signed-off-by: ElishaG <elishajeremy.gonzales@gmail.com>

--- Seight/Seight/defaultPreprocess.cpp
+++ Seight/Seight/defaultPreprocess.cpp
@@ -21,17 +21,19 @@ Mat defaultPreprocess::perform(Mat image)
 	
 	Mat hsv;
 	//imshow("image", image);
-	cvtColor(image, hsv, CV_BGR2HSV);
+	cvtColor(image, hsv, CV_BGR2YCrCb);
 
 	vector<Mat> channels;
 	split(hsv, channels);
 
 	Mat result;
-	equalizeHist(channels[1], channels[1]);
-	equalizeHist(channels[2], channels[2]);
+	equalizeHist(channels[0], channels[0]);
+	//equalizeHist(channels[1], channels[1]);
+	//equalizeHist(channels[2], channels[2]);
 	merge(channels, hsv);
-	cvtColor(hsv, result, CV_HSV2BGR);
+	cvtColor(hsv, result, CV_YCrCb2BGR);
 	imshow("h", hsv);
+	imshow("im", image);
 	imshow("r", result);
 	waitKey(0);
 
